6f5d8a6dca146347e4edc9955c9c33c032d3df18
meirtin <meirtin@gmail.com>
2025-06-12 01:33:35 -0300

    fix swagger docs location file, added serverless lift

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use Illuminate\Support\Facades\Response;
-
-Route::get('/api-docs.json', function () {
-    $path = storage_path('api-docs/api-docs.json');
-    if (!file_exists($path)) {
-        abort(404, 'API docs not found');
-    }
-    return Response::file($path, ['Content-Type' => 'application/json']);
+Route::get('/', function () {
+    abort(401);
 });
